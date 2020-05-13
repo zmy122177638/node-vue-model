ac0113fa89333ce7a40fd62cd820750c3011ea2a
anles <122177638@qq.com>
2020-05-13 16:17:13 +0800

    优化session

--- server/middleware/checkPermission.js
+++ server/middleware/checkPermission.js
@@ -9,8 +9,16 @@ module.exports = () => {
     if (!ctx.session.id && !filterRoutes.some((path) => ctx.path === path)) {
       ctx.throw(400,{code: 3002})
     } else {
-      /** 重写cookie设置 */
-      ctx.cookies.set('SESSION_ID', ctx.cookies.get('SESSION_ID'),{maxAge: config.maxAge})
+      /** 
+       * 重写cookie 
+       * 如果安全级较低可以关闭，maxAge设置时间加长 
+       * 当用户不发送新请求时,maxAge将根据第一次设置的时间设置
+       * 当用户在maxAge的有效期内操作，此设置会实时在当前的时间重新设置maxAge
+       * */
+      const sessionId = ctx.cookies.get('SESSION_ID')
+      if (sessionId) {
+        ctx.cookies.set('SESSION_ID', sessionId, {maxAge: config.maxAge})
+      }
       await next()
     }
   }
--- server/plugins/session/index.js
+++ server/plugins/session/index.js
@@ -8,13 +8,14 @@ let store = new MysqlSession(config.mysql)
 const plugin = session({
   key: 'SESSION_ID',
   store: store,
-  cookie: {
-    domain: 'localhost',  // 写cookie所在的域名
-    path: '/',       // 写cookie所在的路径
-    maxAge: config.maxAge, // cookie有效时长
-    expires: new Date(),  // cookie失效时间
-    httpOnly: false,  // 是否只用于https请求中获取
-    overwrite: false  // 是否允许重写
+  cookie: (ctx) => {
+    return {
+      domain: 'localhost',  // 写cookie所在的域名
+      path: '/',       // 写cookie所在的路径
+      maxAge: config.maxAge, // cookie有效时长
+      httpOnly: false,  // 是否只用于https请求中获取
+      overwrite: false  // 是否允许重写
+    }
   }
 })
 
